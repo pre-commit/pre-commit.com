687151386b3f94bf344e299c2f146c513d985042
Ken Struys <ken@struys.ca>
2014-06-30 10:24:34 -0700

    only push to pre-commit.github.io in travis if the branch is real master

---
+++ push_to_gh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[ $TRAVIS_BRANCH == real_master ] || exit
+
+git clone "https://${GH_TOKEN}@${GH_REF}" out >& /dev/null
+cd out
+git checkout master
+git config user.name "Travis-CI"
+git config user.email "kstruys@yelp.com"
+cp ../.travis.yml .
+cp ../index.html .
+cp -R ../build .
+cp -R ../bower_components .
+git add .
+git commit -m "Deployed ${TRAVIS_BUILD_NUMBER} to Github Pages"
+git push -q origin HEAD >& /dev/null
