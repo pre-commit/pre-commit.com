858241d0cab0aa8dda14bd8d73d0d10a02d93128
Anthony Sottile <asottile@umich.edu>
2017-01-04 12:43:06 -0800

    Faster installation by upgrading virtualenv

--- make_all_hooks.py
+++ make_all_hooks.py
@@ -2,11 +2,8 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import collections
-import contextlib
-import io
 import json
 import os.path
-import shutil
 import subprocess
 import tempfile
 
@@ -14,27 +11,18 @@ import aspy.yaml
 from pre_commit.clientlib.validate_manifest import load_manifest
 
 
-@contextlib.contextmanager
-def tempdir():
-    directory = tempfile.mkdtemp()
-    try:
-        yield directory
-    finally:
-        shutil.rmtree(directory)
-
-
 def get_manifest_from_repo(repo_path):
-    with tempdir() as directory:
+    with tempfile.TemporaryDirectory() as directory:
         repo_dir = os.path.join(directory, 'repo')
-        subprocess.call(['git', 'clone', repo_path, repo_dir])
+        subprocess.call(('git', 'clone', repo_path, repo_dir))
         manifest_path = os.path.join(repo_dir, 'hooks.yaml')
         # Validate the manifest just to make sure it's ok.
         load_manifest(manifest_path)
-        return aspy.yaml.ordered_load(io.open(manifest_path))
+        return aspy.yaml.ordered_load(open(manifest_path))
 
 
 def main():
-    repos = aspy.yaml.ordered_load(io.open('all-repos.yaml'))
+    repos = aspy.yaml.ordered_load(open('all-repos.yaml'))
     hooks_json = collections.OrderedDict()
     for repo in repos:
         hooks_json[repo] = get_manifest_from_repo(repo)
