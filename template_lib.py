3165a4c71bea02edbd90d879225d3ff09729ba37
Anthony Sottile <asottile@umich.edu>
2017-09-17 18:46:12 -0700

    Convert the rest of the index to markdown

--- template_lib.py
+++ template_lib.py
@@ -1,7 +1,28 @@
+import re
+
 import markdown_code_blocks
 import markupsafe
 
 
+ID_RE = re.compile(r'\[\]\(#([a-z0-9-]+)\)')
+SPECIAL_CHARS_RE = re.compile('[^a-z0-9 _-]')
+
+
+class Renderer(markdown_code_blocks.CodeRenderer):
+    def header(self, text, level, raw=None):
+        match = ID_RE.search(raw)
+        if match:
+            h_id = match.group(1)
+        else:
+            h_id = SPECIAL_CHARS_RE.sub('', raw.lower()).replace(' ', '-')
+        return (
+            f'<h{level} id="{h_id}">'
+            f'    {text} <small><a href="#{h_id}">¶</a></small>'
+            f'</h{level}> '
+        )
+
+
 def md(s):
+    html = markdown_code_blocks.highlight(s, Renderer=Renderer)
     # manually bless the highlighted output.
-    return markupsafe.Markup(markdown_code_blocks.highlight(s))
+    return markupsafe.Markup(html)
