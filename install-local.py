180005c08e704677f4396e96b14f80bb7f8a6abf
Max Rozentsveyg <mxr@lyft.com>
2020-05-20 22:07:56 -0400

    Update virtualenv version in install-local.py

--- install-local.py
+++ install-local.py
@@ -20,11 +20,11 @@ else:
 
 
 TGZ = (
-    'https://pypi.python.org/packages/d4/0c/'
-    '9840c08189e030873387a73b90ada981885010dd9aea134d6de30cd24cb8/'
-    'virtualenv-15.1.0.tar.gz'
+    'https://files.pythonhosted.org/packages/a4/e3/'
+    '1f067de470e3a86875ed915438dc3bd781fb0346254f541190a09472b677/'
+    'virtualenv-16.7.10.tar.gz'
 )
-CHECKSUM = '02f8102c2436bb03b3ee6dede1919d1dac8a427541652e5ec95171ec8adbc93a'
+CHECKSUM = 'e88fdcb08b0ecb11da97868f463dd06275923f50d87f4b9c8b2fc0994eec40f4'
 PKG_PATH = '/tmp/.virtualenv-pkg'
 
 
